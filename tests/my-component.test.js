466e3012e829a0c93a28ac7ec9953f81ba5275c2
Rafael Minguet <rminguet@gmail.com>
2017-06-06 19:52:12 +0200

    Day 06-06-2017

--- tests/my-component.test.js
+++ tests/my-component.test.js
@@ -1,15 +1,69 @@
 import test from 'tape';
 
 // Component to test
+const Paginator = function (elementsNumber = 0, numberOfElementsPerPage = 1) {
 
-// Testear que puedes puntuar desde 0 hasta 10 en primera
-test('-------- Component: test ...', (assert) => {
-    const message = 'Message when error';
-    const expected = 1;
+    if (numberOfElementsPerPage < 1) {
+        throw Object.create(RangeError('Number of elements per page cannot be inferior than 1'));
+    }
 
-    const actual = 'component return';
+    let elements = elementsNumber;
+    let elementsPerPage = numberOfElementsPerPage;
 
-    assert.equal(actual, expected, message);
+    const getTotalPages = function () {
+        return Math.ceil(elements / elementsPerPage);
+    };
 
+    return {getTotalPages};
+};
+
+
+// Ideas:
+// Número de páginas totales
+// En qué página estoy
+// Cuál es el previous y el next
+// Cuántos ítems por página
+// Offset de la página actual (el número de elemento del último elemento de la página actual)
+// Número de links que vamos a mostrar (número de páginas a partir de la página actual)
+
+
+test('-------- Component: return total number of pages', (assert) => {
+    const message = 'Wrong number of pages';
+
+    const elementsNumber = 10;
+    const elementsPerPage = 10;
+    const expectedPages = 1;
+
+    const paginator = Paginator(elementsNumber, elementsPerPage);
+    const actualPages = paginator.getTotalPages();
+
+    assert.equal(actualPages, expectedPages, message);
+    assert.end();
+});
+
+test('-------- Component: return total number of pages when total of elements is not a multiple of elements per page', (assert) => {
+    const message = 'Wrong number of pages';
+
+    const elementsNumber = 12;
+    const elementsPerPage = 10;
+    const expectedPages = 2;
+
+    const paginator = Paginator(elementsNumber, elementsPerPage);
+    const actualPages = paginator.getTotalPages();
+
+    assert.equal(actualPages, expectedPages, message);
+    assert.end();
+});
+
+test('-------- Component: elements per page must be greater than 0', (assert) => {
+    const message = 'If we assign 0 or less as the number of elements per page, our Paginator throws a RangeError';
+
+    const elementsNumber = 10;
+    const elementsPerPage = 0;
+    const exceptionExpected = RangeError;
+
+    const actualException = () => Paginator(elementsNumber, elementsPerPage);
+
+    assert.throws(actualException, exceptionExpected, message);
     assert.end();
 });
