c2f40c6ec1aa6a75d04834eae755b37b0d9c97b3
Rafael Minguet <rminguet@gmail.com>
2017-06-13 15:51:59 +0200

    Day 13-06-2017

--- tests/my-component.test.js
+++ tests/my-component.test.js
@@ -1,32 +1,111 @@
 import test from 'tape';
 
-// Component to test
-const Paginator = function (elementsNumber = 0, numberOfElementsPerPage = 1) {
+const Page = function (offsetFirstElement, offsetLastElement) {
+    let nextObject = undefined;
+    let previousObject = undefined;
 
-    if (numberOfElementsPerPage < 1) {
+    const next = function (n = null) {
+        if (null !== n) {
+            nextObject = n;
+        }
+        return nextObject;
+    };
+
+    const previous = function (p = null) {
+        if (null !== p) {
+            previousObject = p;
+        }
+        return previousObject;
+    };
+
+    return {offsetFirstElement, offsetLastElement, next, previous};
+};
+
+// Component to test
+const Paginator = function (elements = 0, elementsPerPage = 1) {
+
+    if (elementsPerPage < 1) {
         throw Object.create(RangeError('Number of elements per page cannot be inferior than 1'));
     }
 
-    let elements = elementsNumber;
-    let elementsPerPage = numberOfElementsPerPage;
+    const totalPages = Math.ceil(elements / elementsPerPage);
+    const pages = [];
+    let currentPage = 1;
+    const MIN_PAGE = 1;
 
-    const getTotalPages = function () {
-        return Math.ceil(elements / elementsPerPage);
+    const getPage = function (index) {
+        return undefined === pages[index] ? pages[index] : null;
     };
 
-    return {getTotalPages};
+    const pagesConstructor = function () {
+        let offsetFirstElement = 0;
+        let offsetLastElement = 0;
+        let newPage = {};
+
+        for (let pagesCounter = MIN_PAGE; pagesCounter <= totalPages; pagesCounter++) {
+
+            offsetLastElement = ((elementsPerPage * pagesCounter) - 1) > (elements - 1)
+                ? elements - 1
+                : (elementsPerPage * pagesCounter) - 1;
+            newPage = Page(offsetFirstElement, offsetLastElement);
+            offsetFirstElement = offsetLastElement + 1;
+
+            pages.push(newPage);
+            if (undefined !== pages[pages.length - 2]) {
+                pages[pages.length - 1].previous(pages[pages.length - 2]);
+                pages[pages.length - 2].next(pages[pages.length - 1]);
+            }
+        }
+    };
+
+    const throwPageErrorException = function (pageNumber) {
+        throw Object.create(RangeError('Page ' + pageNumber + ' doesn\'t exist. Existing pages go from ' +
+            MIN_PAGE + ' to ' + totalPages + '.'));
+    };
+
+    const validPage = function (pageNumber) {
+        return !(pageNumber > totalPages || pageNumber < MIN_PAGE);
+    };
+
+
+    // Public methods
+
+    const getTotalPages = function () {
+        return totalPages;
+    };
+
+    const go = function (pageNumber) {
+        if (!validPage(pageNumber)) {
+            throwPageErrorException(pageNumber);
+        }
+        currentPage = pageNumber;
+    };
+
+    const previousPage = function () {
+        return 0 === (currentPage - 1) ? MIN_PAGE : currentPage - 1;
+    };
+
+    const nextPage = function () {
+        return (currentPage + 1) > totalPages ? totalPages : currentPage + 1;
+    };
+
+    const getCurrentPage = function () {
+        return currentPage;
+    };
+
+    const offsetsRange = function (pageNumber) {
+        if (!validPage(pageNumber)) {
+            throwPageErrorException(pageNumber);
+        }
+        return [pages[pageNumber - 1].offsetFirstElement, pages[pageNumber - 1].offsetLastElement];
+    };
+
+    pagesConstructor();
+
+    return {getTotalPages, go, previousPage, nextPage, currentPage: getCurrentPage, offsetsRange};
 };
 
-
-// Ideas:
-// Número de páginas totales
-// En qué página estoy
-// Cuál es el previous y el next
-// Cuántos ítems por página
-// Offset de la página actual (el número de elemento del último elemento de la página actual)
-// Número de links que vamos a mostrar (número de páginas a partir de la página actual)
-
-
+// TESTS
 test('-------- Component: return total number of pages', (assert) => {
     const message = 'Wrong number of pages';
 
@@ -65,5 +144,105 @@ test('-------- Component: elements per page must be greater than 0', (assert) =>
     const actualException = () => Paginator(elementsNumber, elementsPerPage);
 
     assert.throws(actualException, exceptionExpected, message);
+    assert.end();
+});
+
+test('-------- Component: testing previous page', (assert) => {
+    const message = 'Previous page must be the previous to the current page.';
+
+    const elementsNumber = 12;
+    const elementsPerPage = 10;
+    const expectedPreviousPage = 1;
+
+    const paginator = Paginator(elementsNumber, elementsPerPage);
+    paginator.go(2);
+    const actualPreviousPage = paginator.previousPage();
+
+    assert.equal(actualPreviousPage, expectedPreviousPage, message);
+    assert.end();
+});
+
+test('-------- Component: testing next page', (assert) => {
+    const message = 'Next page must be the next to the current page.';
+
+    const elementsNumber = 12;
+    const elementsPerPage = 10;
+    const expectedPreviousPage = 2;
+
+    const paginator = Paginator(elementsNumber, elementsPerPage);
+    const actualPreviousPage = paginator.nextPage();
+
+    assert.equal(actualPreviousPage, expectedPreviousPage, message);
+    assert.end();
+});
+
+test('-------- Component: testing previous page when we are in the first page', (assert) => {
+    const message = 'Previous page when we are in the first page must be the first page.';
+
+    const elementsNumber = 12;
+    const elementsPerPage = 10;
+    const expectedPreviousPage = 1;
+
+    const paginator = Paginator(elementsNumber, elementsPerPage);
+    const actualPreviousPage = paginator.previousPage();
+
+    assert.equal(actualPreviousPage, expectedPreviousPage, message);
+    assert.end();
+});
+
+test('-------- Component: testing next page when we are in the last page', (assert) => {
+    const message = 'Next page when we are in the last page must be the last page.';
+
+    const elementsNumber = 12;
+    const elementsPerPage = 10;
+    const expectedPreviousPage = 2;
+
+    const paginator = Paginator(elementsNumber, elementsPerPage);
+    const actualPreviousPage = paginator.nextPage();
+
+    assert.equal(actualPreviousPage, expectedPreviousPage, message);
+    assert.end();
+});
+
+test('-------- Component: we cannot navigate to an unexisting page', (assert) => {
+    const message = 'The page we want to navigate doesn\'t exist and we cannot navigate to it.';
+
+    const elementsNumber = 12;
+    const elementsPerPage = 10;
+    const exceptionExpected = RangeError;
+
+    const paginator = Paginator(elementsNumber, elementsPerPage);
+    const actualValue = () => paginator.go(3);
+
+    assert.throws(actualValue, exceptionExpected, message);
+    assert.end();
+});
+
+test('-------- Component: asking for the current page', (assert) => {
+    const message = 'That is not the current page';
+
+    const elementsNumber = 14;
+    const elementsPerPage = 10;
+    const expectedPage = 2;
+
+    const paginator = Paginator(elementsNumber, elementsPerPage);
+    paginator.go(paginator.nextPage());
+    const actualPage = paginator.currentPage();
+
+    assert.equal(actualPage, expectedPage, message);
+    assert.end();
+});
+
+test('-------- Component: calculate offsets of the first and the last element of a page', (assert) => {
+    const message = 'Offsets means what index takes the last and the first item of a page on the total of elements.';
+
+    const elementsNumber = 14;
+    const elementsPerPage = 10;
+    const expectedRangeOffsets = [10, 13];
+
+    const paginator = Paginator(elementsNumber, elementsPerPage);
+    const actualRangeOffsets = paginator.offsetsRange(2);
+
+    assert.deepEqual(actualRangeOffsets, expectedRangeOffsets, message);
     assert.end();
 });
